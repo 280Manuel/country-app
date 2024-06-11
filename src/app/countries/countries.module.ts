b4ca70ed65652be32fedf3f075c6565ddb5d76c4
Manuel Sanchez <manuelsf280@gmail.com>
2024-06-10 18:04:18 -0700

    primer commit

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCApitanPageComponent } from './pages/by-capitan-page/by-capitan-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ByCApitanPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
---
+++ src/app/countries/pages/by-capitan-page/by-capitan-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capitan-page',
+  templateUrl: './by-capitan-page.component.html',
+  styles: ``
+})
+export class ByCApitanPageComponent {
+
+}
